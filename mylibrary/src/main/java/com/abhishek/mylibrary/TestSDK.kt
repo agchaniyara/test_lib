9a98d0a75a4187c1a0330042b486a3778484cbe5
Abhishek Chaniyara <chaniyara.abhishek@gmail.com>
2022-11-23 00:12:54 +0530

    - make test sdk public

--- mylibrary/src/main/java/com/abhishek/mylibrary/TestSDK.kt
+++ mylibrary/src/main/java/com/abhishek/mylibrary/TestSDK.kt
@@ -2,7 +2,7 @@ package com.abhishek.mylibrary
 
 import android.util.Log
 
-object TestSDK : TestListener {
+public object TestSDK : TestListener {
     override fun addVariable(first: Int, second: Int) {
         Log.d("Test SDK", "Addition of two variable is :" + (first + second))
     }
