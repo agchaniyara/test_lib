7d79782ff785df2ac0fec0cd62518c256cbea13c
Abhishek Chaniyara <chaniyara.abhishek@gmail.com>
2022-11-23 00:28:44 +0530

    - make test sdk final build

--- mylibrary/src/main/java/com/abhishek/mylibrary/TestSDK.kt
+++ mylibrary/src/main/java/com/abhishek/mylibrary/TestSDK.kt
@@ -3,7 +3,7 @@ package com.abhishek.mylibrary
 import android.util.Log
 
 public object TestSDK : TestListener {
-    override fun addVariable(first: Int, second: Int) {
+    public override fun addVariable(first: Int, second: Int) {
         Log.d("Test SDK", "Addition of two variable is :" + (first + second))
     }
 }
